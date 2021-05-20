ec900840be4d5fb1671a86223b05c2e4e07f7fa8
shubhamaiocd <84322116+shubhamaiocd@users.noreply.github.com>
2021-05-20 14:31:33 +0530

    Update BucketCon.py

--- file_convert/BucketHandler/BucketCon.py
+++ file_convert/BucketHandler/BucketCon.py
@@ -4,9 +4,9 @@ from google.cloud import storage
 class BucketConfig:
 
     def __init__(self, awacslogger) -> None:
-        # self.__storage_client = storage.Client()
-        self.__storage_client = storage.Client.from_service_account_json(
-            'C:/Users/Shubham Snehi/Downloads/awacs-dev-3543bb21996e.json')
+        self.__storage_client = storage.Client()
+        #self.__storage_client = storage.Client.from_service_account_json(
+        #    'C:/Users/Shubham Snehi/Downloads/awacs-dev-3543bb21996e.json')
         self.__awacslogger = awacslogger
 
     # Cloud Storage Bucket Methods
