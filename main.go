0a18b5f008e202ab976114b81a5042d99f0c2eae
Ganesh Kale <ganesh@aiocdawacs.com>
2021-06-10 20:44:27 +0530

    change mutex in goroutines

--- main.go
+++ main.go
@@ -136,7 +136,7 @@ func main() {
 }
 
 func worker(ctx context.Context, filename string, bucketname string) {
-	log.Printf("Receved file : %v\n", filename)
+	log.Printf("Receved file in worker : %v\n", filename)
 	// if msg.Attributes["eventType"] == "OBJECT_DELETE" {
 	// 	msg.Ack()
 	// 	return
