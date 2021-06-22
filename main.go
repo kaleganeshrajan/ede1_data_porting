b2ea64ad998939192962dfcf709976bcb42717d6
Ganesh Kale <ganesh@aiocdawacs.com>
2021-06-22 14:59:33 +0530

    date format change 3

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ type BukectStruct struct {
 func init() {
 	//awacsSubNames = []string{"awacs-ede1-test-sub"}
 	//projectID = "awacs-dev"
-	maxGoroutines = 1
+	maxGoroutines = 15
 }
 
 func main() {
